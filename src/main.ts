b85b017d74d9c32746c1fcefc5aea5237125b49f
BrennerBatR <brenner.batista@outlook.com>
2020-10-14 16:16:57 -0300

    port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = Number(process.env.PORT) || 3000;
+  await app.listen(PORT, '0.0.0.0');
+  console.log('Base64 converter is listening port : ', PORT);
 }
 bootstrap();
