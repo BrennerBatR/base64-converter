d38d3d55d47e70f4cd475738868ecac424cbe928
BrennerBatR <brenner.batista@outlook.com>
2020-10-14 16:05:24 -0300

    file to base64

---
+++ src/app.controller.ts
@@ -0,0 +1,38 @@
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UploadedFile,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express/multer/interceptors/file.interceptor';
+import { FilesInterceptor } from '@nestjs/platform-express/multer/interceptors/files.interceptor';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('upload')
+  @UseInterceptors(FilesInterceptor('files'))
+  uploadFile(@UploadedFiles() files) {
+    let img = [];
+    for (let f of files) {
+      let base64 =
+        'data:' +
+        f.mimetype +
+        ';base64,' +
+        Buffer.from(f.buffer).toString('base64');
+      img.push(base64);
+    }
+    return { img };
+  }
+}
